69824c4db50dd98dd8a2bd62712eb65c9c7437a7
Vivaldo Hermes <vivaldostein@gmail.com>
2025-09-06 21:50:51 -0300

    feat-api/add post transfer endpoint

---
+++ tokio-transfer-api/src/main/java/com/tokiomarine/controller/TransferController.java
@@ -0,0 +1,32 @@
+package com.tokiomarine.controller;
+
+import com.tokiomarine.dto.TransferRequestDTO;
+import com.tokiomarine.dto.TransferResponseDTO;
+import com.tokiomarine.service.TransferService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+
+@RestController
+@RequestMapping("/transfers")
+public class TransferController {
+
+    private final TransferService transferService;
+
+    public TransferController(TransferService transferService) {
+        this.transferService = transferService;
+    }
+
+    @PostMapping
+    public ResponseEntity<TransferResponseDTO> create(@RequestBody @Valid TransferRequestDTO body) {
+        TransferResponseDTO created = transferService.create(body);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(created.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(created);
+    }
+}
